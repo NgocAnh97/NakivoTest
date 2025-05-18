1e4167c38107b454ee085301409c4ecdb813c8dc
NgocAnh <dtngocanh97@gmail.com>
2025-05-18 15:28:17 +0700

    update config files

--- src/main/java/com/nakivo/constants/FrameworkConstants.java
+++ src/main/java/com/nakivo/constants/FrameworkConstants.java
@@ -16,4 +16,6 @@ public final class FrameworkConstants {
     public static final int WAIT_PAGE_LOADED = Integer.parseInt(PropertiesHelpers.getValue("WAIT_PAGE_LOADED"));
     public static final int WAIT_EXPLICIT = Integer.parseInt(PropertiesHelpers.getValue("WAIT_EXPLICIT"));
     public static final String PROJECT_PATH = SystemHelpers.getCurrentDir();
+    public static final String REPORT_TITLE = PropertiesHelpers.getValue("REPORT_TITLE");
+    public static final String AUTHOR = PropertiesHelpers.getValue("AUTHOR");
 }
